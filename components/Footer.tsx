b8ea6158b3a6e4f19a9fe1f46d0b6590fa091c31
juniorrondini <jjrondinijj@gmail.com>
2025-01-27 07:48:12 -0300

    Revert "Google"
    This reverts commit 2fa2c70b55363d80a7f5eb30c2b01e49445f5340.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,6 @@ import Link from "next/link"
 
 export default function Footer() {
   return (
-    
     <footer className="border-t border-cyan-700 bg-black/50 backdrop-blur-sm">
       <div className="container mx-auto flex items-center justify-between p-4">
         <p className="text-cyan-400">&copy; 2023 CyberQuiz. All rights reserved.</p>
@@ -21,7 +20,6 @@ export default function Footer() {
           </ul>
         </nav>
       </div>
-      <meta name="google-adsense-account" content="ca-pub-8997726626133132"></meta>
     </footer>
   )
 }
